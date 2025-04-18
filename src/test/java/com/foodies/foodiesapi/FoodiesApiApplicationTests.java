d3652d32a12c557773a722719f26f6c83bc644cd
Fatima Zahra El Mouden <elmoudenfatimazahra01@gmail.com>
2025-04-18 12:04:17 +0100

    first commit

---
+++ src/test/java/com/foodies/foodiesapi/FoodiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodies.foodiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
